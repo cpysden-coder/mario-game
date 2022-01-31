9411baeab2d84796bbf6d086a3e9114ce6164d57
cpysden-coder <chris_pysden@mac.com>
2022-01-31 15:33:42 -0800

    completed player movement up,left,right initial

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ class Player {
     update(){
         this.draw()
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity
         else this.velocity.y = 0
@@ -39,10 +40,77 @@ class Player {
 
 const player = new Player();
 
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     // console.log('go')
+    if(keys.right.pressed){
+        player.velocity.x = 5
+    } else if(keys.left.pressed) {
+        player.velocity.x = -5} else player.velocity.x = 0
     player.update()
 }
 animate()
+
+//add eventListeners at bottom
+addEventListener('keydown', ({key, keyCode}) => {
+    console.log(`key: ${key} and keycode: ${keyCode}`)
+    switch(keyCode){
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+        break;
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+        break;
+        
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20;
+
+        break;
+        
+        case 83:
+            console.log('down')
+        break;
+    }
+    
+})
+
+addEventListener('keyup', ({key, keyCode}) => {
+    console.log(`key: ${key} and keycode: ${keyCode}`)
+    switch(keyCode){
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+        break;
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+        break;
+        
+        case 87:
+            console.log('up')
+            // player.velocity.y -= 20;
+
+        break;
+        
+        case 83:
+            console.log('down')
+        break;
+
+    }
+    
+})
